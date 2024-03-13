fa5b7f92d6130b99eb5ae4f81589853306afa30a
VolffS <volff-v@mail.ru>
2024-03-13 13:43:28 +0300

    begin

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Header from "./components/header/header.tsx";
+import {MainContent} from "./components/main-content/main-content.tsx";
+
+export const App = () => {
+
+    return (<>
+            <Header />
+            <MainContent />
+    </>
+    );
+}
+
+
+
+
